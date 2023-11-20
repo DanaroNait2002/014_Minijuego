a054811f13b23139027b9b9a991fa06d9467a560
Daniela Rodríguez <danaronait2002@gmail.com>
2023-11-20 13:35:05 +0000

    Commit_027
    Pocas cosas, la verdad

---
+++ Assets/Scripts/Game/Tutorial/Tutorial.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    #region VARIABLES
+
+    [SerializeField]
+    private GameObject[] steps;
+    [SerializeField]
+    private int value;
+
+    #endregion
+
+    #region METHODS
+
+
+
+    #endregion
+}
